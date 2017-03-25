08a21763288a71a7457923fe8c3163c4c6958303
Phodal HUANG <h@phodal.com>
2017-03-25 09:22:44 +0800

    [T] basic test with eventstorming

--- src/app/containers/Home/index.tsx
+++ src/app/containers/Home/index.tsx
@@ -1,8 +1,27 @@
 import * as React from 'react';
+import {EventBusiness, EventEntity, EventStickyRender, EventSubscriber} from 'eventstorming';
 const style = require('./style.css');
 
 class Home extends React.Component<any, any> {
   public render() {
+
+    const eventSubscriber = new EventSubscriber();
+    const eventStickyRender = new EventStickyRender(eventSubscriber);
+
+    const eventBusiness = new EventBusiness(eventSubscriber);
+    const subEntity = new EventEntity('sticker had store');
+
+    const eventEntity = eventBusiness.createEventSticky('event had created');
+    eventEntity.addRelatedChild(subEntity);
+    eventBusiness.updateEventSticky(eventEntity);
+
+    eventBusiness.createEventSticky('sticky had created');
+    eventBusiness.createEventSticky('sticky had rendered');
+
+    const result = eventStickyRender.render();
+
+    console.log(result);
+
     return (
       <div className={style.Home}>
         <img src={require('./barbar.png')} />
